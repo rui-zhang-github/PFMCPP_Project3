3236720c21e81c1f5ead83f345e3aa6ebad0f3c6
matkatmusic <matkatmusic@gmail.com>
2022-01-16 08:30:44 -0800

    Added part 1e

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
-Project 3 - Part 1d / 5
+Project 3 - Part 1e / 5
 Video:  Chapter 2 Part 5
 User-Defined Types
 
@@ -12,8 +12,8 @@ What you create in this project will be used as the basis of Project 5 in the co
 ************************
 Part1 purpose:  Learn to write UDTs
 
-You are going to write 10 UDTs in project3. 
-Part1 will be broken up into 5 separate steps 
+You are going to write 10 UDTs in project3.  
+Part1 will be broken up into 5 separate steps
     Part 1a: you will learn to think about an object in terms of its sub-objects.
     Part 1b: you will write 4 un-related UDTs in plain english
     Part 1c: you will write 1 UDT in plain english that will be made of 5 related sub-objects
@@ -21,68 +21,170 @@ Part1 will be broken up into 5 separate steps
     Part 1e: you will convert those 10 plain-english UDTs into code that runs.
 ************************
 
-    The goal of this step is to get you to think about breaking down an object into smaller and smaller objects, 
-    until the smallest object is made of up only C++ primitives and std::string. 
+Convert your 10 Plain-english UDTs into code.
 
-    Continuing the previous example:  Cell Phone
+I recommend compiling after finishing each one and making sure it compiles 
+without errors or warnings before moving on to writing the next UDT. 
 
-    A Cell Phone is made up of the following 5 properties/sub-objects and 3 actions:
-        Display
-        Memory
-        CPU
-        Radio
-        Applications
-    3 actions:
-        make a call
-        send a text
-        run an application.
+1) define an empty struct below your plain-english UDT. i.e.:
 
-    These 5 properties can be broken down into their own sub-objects and properties. 
+Thing: Car Wash   
+    5 properties:
+        - number of vacuum cleaners
+        - number of eco-friendly cleaning supplies
+        - stores the amount of water used per week.
+        - stores amount of profit made per week
+        - number of cars serviced per day
+    3 things it can do:
+        - wash and wax car
+        - charge customer
+        - detail the car interior
+ */
 
-    If we break down the first property 'Display' into its 5 properties we get:
-        brightness
-        amount of power consumed.
-        pixels
-        width in cm
-        height in cm
+#if false //ignore these #if #endif lines. they're just here to prevent compiler errors.
+struct CarWash
+{
 
-    the Display's brightness can be represented with a Primitive, such as a double. 
+};
+#endif
+/*
+    - Do this for all 10 UDTs
 
-    The amount of power consumed can also be represented with a Primitive, such as a float or integer (i.e. 250mWa)
+2) Below your plain-english UDT, Copy your 5 properties & 3 actions into the empty struct body.
+    - comment them out.
+    - Do this for all 10 UDTs
     
-    The 'pixels' property must be represented with an array of Pixel instances, as the screen has more than 1 row of pixels.
-        Arrays have not been discussed and can't be used in this project.
-        Instead, we can use an Integer primitive to store the Number of Pixels:
-
-    Display:
-        Number of Pixels
-        Amount of Power consumed (milliwatt-hours)
-        Brightness
-        width in cm
-        height in cm
-************************
-
-1) Fill in #5 - #9 with plain-english UDTs for the 5 properties you created for UDT #10
-    example: 
-        If #10's first property was 'Engine', then your `Thing 5)` will be `Engine`
-        you will need to provide 5 properties and 3 member functions of that Engine object in plain English
-        Remember to pick properties that can be represented with 'int float double bool char std::string'
-2) write the name of the primitive type you'll be using after each property
-    pick properties that can be represented with 'int float double bool char std::string'
-    example: 
-        Display:
-            Number of Pixels (int)
-            Amount of Power consumed (milliwatt-hours) (float)
-            Brightness (double)
-            width in cm (int)
-            height in cm (int)
-
-3) move Thing 5-9 to between your Thing 4 and Thing 10.
-*/
+3) declare your member variables and member functions underneath each plain-english comment in your struct's body.
+    - give the member variables relevant data types
+    - Do this for all 10 UDTs
+    - if your functions return something other than 'void', add a comment explaining what is being returned.  see the example code below.
+ 
+4) make the function parameter list for those member functions use some of your User-Defined Types
+    - You'll write definitions/implementations for these functions in Project3 Part2
+    - you'll call each of these functions in Project3 part3
+    - Do this for all 10 UDTs
+ 
+5) make 2 of the 10 user-defined types have a nested class.  
+    - this nested class also needs 5 properties and 3 actions.
+    - these nested classes are not considered one of your 10 UDTs.
+    - this nested class must be related to the class it is nested inside
+ 
+6) your 10th UDT's properties should be instances of your #5-#9 UDTs.   
+    - No primitives allowed!
+ 
+7) After you finish defining each type, click the [run] button.  
+    Clear up any errors or warnings as best you can. 
+    if your code produces a [-Wpadded] warning, add '-Wno-padded' to the .replit file with the other compiler flags (-Weverything -Wno-missing-prototypes etc etc)
+ */
 
 /*
-Thing 5-9 have been moved to between Thing 4 and 10.
+ example:  
+
+Thing: Car Wash   
+    5 properties:
+        - number of vacuum cleaners
+        - number of eco-friendly cleaning supplies
+        - stores the amount of water used per week.
+        - stores amount of profit made per week
+        - number of cars serviced per day
+    3 things it can do:
+        - wash and wax car
+        - charge customer
+        - detail the car interior
  */
+
+#include <iostream>
+#include <string>
+
+struct CarWash //                                   1) define an empty struct for each of your 10 types.       
+{
+    //number of vacuum cleaners                     2) copied and commented-out plain-english property
+    int numVacuumCleaners = 3; //                   3) member variables with relevant data types.
+    //number of eco-friendly cleaning supplies      
+    int numEcoFriendlyCleaningSupplies = 20;     
+    //stores the amount of water used per week.     
+    float waterUsedPerWeek = 200.f;            
+    //stores amount of profit made per week         
+    float profitPerWeek = 495.95f;               
+    //number of cars serviced per day               
+    int numberOfCarsServiced = 10;               
+    
+    struct Car //5)                                 Note that the nested type 'Car' is related to the 'CarWash' 
+    {
+        //2) member variables with relevant data types.  the names are appropriate for the U.D.T.'s purpose.
+        bool isAPickupTruck = false;
+        float gasMileage = 26.2f;        
+        int year = 1985;
+        std::string manufacturer = "Toyota";
+        std::string model = "Corolla";
+
+        //3) a member function whose parameter has a default value.
+        //the parameter name is related to the work the function will perform.
+        void fillTank(double fuelAmountInGallons = 2.0);  
+        void breakDown(std::string failureType, bool requiresTow = false);
+        int getMilesTraveledAnnually(bool includeUberLyftTrips); //3) returns the number of miles traveled
+    };
+
+    //wash and wax car
+    void washAndWaxCar(Car carA); //4) a member function whose parameter is a UDT.
+    //charge customer
+    float chargeCustomer(float discountPercentage); //3) returns the total amount charged.
+    //detail the car interior
+    void detailInterior(Car carB);
+    
+    //5) a member variable whose type is a UDT.
+    Car carBeingServiced;  
+
+    /*
+    Pay attention to the member functions that take an instance of 'Car'
+    Notice that there is a member variable of the same type.
+
+    It makes sense to pass a Car to the function 'washAndWaxCar' because car washes service MANY cars
+    However, they only service ONE car at a time.
+    the carBeingServiced's value would change every time you wash and wax the car. 
+
+    I see many students who write code emulating this format, but their usage does not make logical sense.  
+    Consider the following snippet:
+
+    struct SoccerTeam
+    {
+        struct Manager
+        {
+            ...
+        };
+
+        // train their skills
+        void trainPlayerSkills(Manager managerA);
+    
+        Manager teamManager;
+    };
+
+    It does not make sense to pass in a new Manager whenever you are going to train your team players.
+    Soccer teams have ONE manager.
+
+    a much more relevant usage would be adding a member function that hires a new manager:
+
+    struct SoccerTeam
+    {
+        struct Manager
+        {
+            ...
+        };
+
+        void hireNewManager(Manager newManager);
+    
+        Manager teamManager;
+    };
+
+    We can safely assume that the 'Manager' instance that is being passed in will be replacing the current 'teamManager' variable without looking at any other code.
+    This is because the function name and function argument clearly indicate what they are/what they do.
+
+    Your function names and parameter names should make LOGICAL SENSE.
+    Readers of your code should be able to INTUITIVELY understand what your function implementations will do without actually seeing the implementations.
+
+    Keep this in mind when you define your UDTs in this project part.
+    */
+};
 
 /*
 Thing 1) Hotel
